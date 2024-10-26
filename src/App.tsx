c20363b1a81969d0db5fb0a93b54062d027ab1a1
kaykaym01 <kaymoore01@gmail.com>
2024-10-26 16:23:56 -0400

    Fix persisting todos in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,29 @@ import TodoList from "./components/TodoList";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [todos, setTodos] = useState<string[]>([]);
+  const [todos, setTodos] = useState<string[]>(() => {
+    const localTodos = localStorage.getItem("todos");
+    return localTodos ? JSON.parse(localTodos).todos : [];
+  });
   const [todoValue, setTodoValue] = useState<string>("");
-
-  function persistData(newList: string[]) {
-    localStorage.setItem("todos", JSON.stringify({ todos: newList }));
-  }
 
   function addTodoItem(todoItem: string) {
     setTodos(todos.concat([todoItem]));
-    persistData(todos);
   }
 
   function editTodoItem(todoIndex: number) {
     const currentTodoItem = todos[todoIndex];
     setTodoValue(currentTodoItem);
     deleteTodoItem(todoIndex);
-    persistData(todos);
   }
 
   function deleteTodoItem(todoIndex: number) {
     setTodos([...todos.splice(0, todoIndex), ...todos.splice(todoIndex + 1)]);
-    persistData(todos);
   }
 
   useEffect(() => {
-    if (!localStorage) {
-      return;
-    }
-
-    let localTodos = localStorage.getItem("todos");
-    if (!localTodos) {
-      return;
-    }
-
-    localTodos = JSON.parse(localTodos).todos;
-    setTodos(localTodos);
-  }, []);
+    localStorage.setItem("todos", JSON.stringify({ todos }));
+  }, [todos]);
 
   return (
     <>
